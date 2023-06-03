055ba472be09f55caf618a9e648e0204c3b3fc12
Shokhrukh <shoxrux20888@mail.ru>
2023-06-03 17:11:11 +0300

    addCLI
    Выводит справочную информацию

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 gendiff:
-			node bin/gendiff.js
+		node bin/gendiff.js
 publish:
-			npm publish --dry-run	
+		npm publish --dry-run	
 lint:
-			npx eslint
+		npx eslint
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log("Hello World!");
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1')
+    .option('-h, --help', 'output usage information')
+    .parse();
+
+program.help();
