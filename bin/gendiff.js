e2d85c78b60f37aff924587de8c24e84ad242a77
Shokhrukh <shoxrux20888@mail.ru>
2023-06-03 17:30:06 +0300

    addArguments
    Добавил функицю форматирования и возможность передачи аргументов

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,10 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .argument('<filepath1>', '')
+    .argument('<filepath2>', '')
     .option('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
     .parse();
 
 program.help();
